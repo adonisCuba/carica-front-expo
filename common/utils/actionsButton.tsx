e64b8860d8d8d7f7960ff76dd8bf455669b41c14
Adonis Gonzalez Santos <adonis.cuba@gmail.com>
2025-05-15 17:56:17 -0400

    Fix

--- common/utils/actionsButton.tsx
+++ common/utils/actionsButton.tsx
@@ -1,35 +1,21 @@
-import * as Linking from 'expo-linking';
-import { Alert } from 'react-native';
+import * as Linking from "expo-linking";
+import { Alert } from "react-native";
 
 export class ActionsButton {
-
-    public static async actionCallButton(number: string) {
-        try {
-            const supported = await Linking.canOpenURL(`tel:${number}`);
-
-            if (!supported) {
-                Alert.alert('No se puede realizar llamado en este momento')
-            }
-            Linking.openURL(`tel:+${number}`);
-        } catch (error) {
-            Alert.alert('Error al realizar llamado');
-
-        }
-
+  public static async actionCallButton(number: string) {
+    try {
+        console.log("number", number);
+      Linking.openURL(`tel:${number}`);
+    } catch (error) {
+      Alert.alert("Error al realizar llamado");
     }
+  }
 
-    public static async actionWhatsappButton(number: string) {
-        try {
-            const supported = await Linking.canOpenURL(`whatsapp://send?phone=${number}`);
-
-            if (!supported) {
-                Alert.alert('No se puede dirigir a whatsapp en este momento')
-            }
-            Linking.openURL(`whatsapp://send?phone=${number}`);
-        } catch (error) {
-            Alert.alert('Error en la acción');
-
-        }
+  public static async actionWhatsappButton(number: string) {
+    try {
+      Linking.openURL(`whatsapp://send?phone=${number}`);
+    } catch (error) {
+      Alert.alert("Error en la acción");
     }
+  }
 }
-        
